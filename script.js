e813e4d5ccd67feb267b46ece88e6b485c712c29
Satadal Banerjee <satadalbanerjeebiz@gmail.com>
2022-12-19 17:31:35 +0530

    calender responsiveness

--- script.js
+++ script.js
@@ -32,17 +32,17 @@ var typed =new Typed (".re-typing-2",{
     loop:true,
 });
 
-GitHubCalendar(".calendar", "SatadalBanerjee");
+// GitHubCalendar(".calendar", "SatadalBanerjee");
 
-    // or enable responsive functionality:
-    GitHubCalendar(".calendar", "SatadalBanerjee", { responsive: true });
+//     // or enable responsive functionality:
+//     GitHubCalendar(".calendar", "SatadalBanerjee", { responsive: true });
 
-    // Use a proxy
-    GitHubCalendar(".calendar", "SatadalBanerjee", {
-       proxy (username) {
-         return fetch(`https://your-proxy.com/github?user=${SatadalBanerjee}`)
-       }
-    }).then(r => r.text())
+//     // Use a proxy
+//     GitHubCalendar(".calendar", "SatadalBanerjee", {
+//        proxy (username) {
+//          return fetch(`https://your-proxy.com/github?user=${SatadalBanerjee}`)
+//        }
+//     }).then(r => r.text())
 
 
     function contshow4(){  
